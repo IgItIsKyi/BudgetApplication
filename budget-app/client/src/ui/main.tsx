d7ec44f5a6441067a42ee904378a640f46c9ed72
Matthew Diaz <diazmatt2004@gmail.com>
2025-12-14 18:59:39 -0500

    error fix

--- budget-app/client/src/ui/main.tsx
+++ budget-app/client/src/ui/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import DTIRatio from '../components/homepage/dtiratio.tsx'
 import Title from '../components/title.tsx';
 import Registration  from '../components/Launchpage/registration.tsx';
-
-import Registration  from '../components/registration.tsx';
 import Bills from '../components/homepage/bills.tsx'
 import './App.css'
 
