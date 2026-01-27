0ed10f8c498bc5b7c1ed214dda003049c51469ed
Matthew Diaz <diazmatt2004@gmail.com>
2026-01-26 21:23:03 -0500

    Income component added

--- budget-app/client/src/ui/main.tsx
+++ budget-app/client/src/ui/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import Title from '../components/title.tsx';
 import Registration  from '../components/Launchpage/registration.tsx';
 import Bills from '../components/homepage/bills.tsx'
+import Income from '../components/homepage/income.tsx'
 import './App.css'
 import Users from '../components/Launchpage/Users.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Title />
+    <Income />
     <Bills />
     <Users />
     <Registration />
